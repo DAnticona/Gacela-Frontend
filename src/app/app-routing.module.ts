e1b7e5c06fdcb1b95511c83be24758f2d50086f6
danticona <danticona@wollcorp.com>
2019-06-11 17:44:31 -0500

    bootstrap y font-awesome instalados, componentes creados y rutas definidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { WelcomePageComponent } from './componentes/welcome-page/welcome-page.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'not-found', component: NotFoundPageComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { WelcomePageComponent } from './componentes/welcome-page/welcome-page.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    WelcomePageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
